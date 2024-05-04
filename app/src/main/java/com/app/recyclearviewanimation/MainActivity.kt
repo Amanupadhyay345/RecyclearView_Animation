3e0af07bac1c80d6980ad5ae5df066f2b84d2dc5
Aman upadhyay <amanupadhyay231306@gmail.com>
2024-05-04 16:08:28 +0530

    THIS IS MY second commit

--- app/src/main/java/com/app/recyclearviewanimation/MainActivity.kt
+++ app/src/main/java/com/app/recyclearviewanimation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -25,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         binding.mainRecyclearview.adapter = MainAdapter()
 
         binding.Animation.setOnClickListener {
+
+            Toast.makeText(this,"hello github",Toast.LENGTH_LONG).show()
             loadAnimation(binding.mainRecyclearview)
         }
 
